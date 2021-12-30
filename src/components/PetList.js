cbbaa13dd4e79d47b028c26277e6a4fd6fe5b855
Sala Yoshida <yoshidasala@gmail.com>
2021-12-30 18:30:20 -0500

    pets component rendere with select dropdown and filter

--- src/components/PetList.js
+++ src/components/PetList.js
@@ -12,28 +12,50 @@ const cody = {
 // PetList only renders one SinglePet. We'd like it to render a list of pets,
 // passed in as props.pets. Don't forget to add a unique key to each one!
 class PetList extends React.Component {
-
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
-    pets:this.props.pets
+      pets: this.props.pets,
+      category:"all"
+    };
+    this.category = this.category.bind(this)
 
-    }
+  }
+
+
+  category(event) {
+    this.setState({category: event.target.value})
+
 
   }
   render() {
+    let pets = this.props.pets
+    if (this.state.category === "dogs") {
+      pets = this.props.pets.filter((curr) => {
+        return curr.species === "dog"
+      })
+    } else if (this.state.category === "cats") {
+      pets = this.props.pets.filter((curr) => {
+        return curr.species === "cat"
+      })
+    }
+
 
     return (
       <>
         <div className="pet-list">
-          <div>{this.props.pets.map((pet) => {
-            return <SinglePet key={pet.id} pet={pet}/>
-          })}
+          <div>
+            {pets.map((pet) => {
+              return <SinglePet key={pet.id} pet={pet} />;
+            })}
           </div>
 
+          <select onChange={this.category} id="category">
+          <option value="all">all</option>
+            <option value="cats">cats</option>
+            <option value="dogs">dogs</option>
 
-
-
+          </select>
         </div>
       </>
     );
